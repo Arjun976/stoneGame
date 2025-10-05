f9d6d48b34c90a1684a4ae6527a3b630a016cc4e
SHIBIN K A <shibin.k.a6235@gmail.com>
2025-10-05 18:43:36 +0530

    feat: Implement game win condition and scoreboard

--- game/src/pages/Home.jsx
+++ game/src/pages/Home.jsx
@@ -18,9 +18,14 @@ function Home() {
     label: '',
     value: null
   })
-  const [win, setWin] = useState(null)
+  const [userWin, setUserWin] = useState(null)
+  const [systemWin, setSystemWin] = useState(null)
   const [scoreCountUser,setScoreCountUser] = useState(0)
   const [scoreCountSystem,setScoreCountSystem] = useState(0)
+  const [scoreBoard, setScoreBoard] = useState({
+    user:0,
+    system:0
+  })
 
 
    const images = [paper1,rock1, scissor1]
@@ -42,10 +47,6 @@ function Home() {
     setSystemSelect({label: getLabel(randomNumber), value: images[randomNumber]})
    }
 
-   const ShowAnimataion = ()=>{
-    return  <Confetti />
-   }
-
    useEffect(()=>{
     if(select.label==='paper' && systemSelect.label === 'rock')setScoreCountUser((prev)=>prev+1)
     if(select.label==='rock' && systemSelect.label === 'scissor')setScoreCountUser((prev)=>prev+1)
@@ -59,33 +60,39 @@ function Home() {
 
    useEffect(()=>{
     if(scoreCountUser===5){
-      setWin(true)
+      setScoreBoard((prev)=>{
+        return {user: prev.user+1,system:prev.system}
+      })
+      setUserWin(true)
       setScoreCountSystem(0)
       setScoreCountUser(0)
       setTimeout(()=>{
-        setWin(null)
+        setUserWin(false)
     },10000)
     }
     if(scoreCountSystem===5){
-      setWin(false)
+      setScoreBoard((prev)=>{
+        return {user: prev.user,system:prev.system+1}
+      })
+      setSystemWin(true)
       setScoreCountSystem(0)
       setScoreCountUser(0)
       setTimeout(()=>{
-        setWin(null)
+         setSystemWin(false)
     },10000)
     }
    },[scoreCountUser,scoreCountSystem])
 
 
   return (
-    <div className="p-4 w-svw h-svh flex items-center justify-center relative">
-      <div className={`p-2 absolute -top-20 w-[30svw] rounded-sm bg-green-500/30 border border-green-400 flex items-center transition-all ease-linear duration-500 gap-2 ${win?'top-10':'-top-20'}`}>
+    <div className="p-4 w-svw h-svh flex flex-col items-center justify-center relative">
+      <div className={`p-2 absolute -top-20 w-[30svw] rounded-sm bg-green-500/30 border border-green-400 flex items-center transition-all ease-linear duration-500 gap-2 ${userWin?'top-10':'-top-20'}`}>
         <svg viewBox="0 0 24 24" fill="currentColor" className="size-6">
           <path d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z" />
         </svg>
         <span>Win</span>
       </div>
-      <div className={`p-2 absolute -top-20 w-[30svw] rounded-sm bg-red-500/30 border border-red-400 flex items-center transition-all ease-linear duration-500 gap-2 ${(!win&&win!==null)?'top-10':'-top-20'}`}>
+      <div className={`p-2 absolute -top-20 w-[30svw] rounded-sm bg-red-500/30 border border-red-400 flex items-center transition-all ease-linear duration-500 gap-2 ${systemWin?'top-10':'-top-20'}`}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
           <path d="M15.73 5.5h1.035A7.465 7.465 0 0 1 18 9.625a7.465 7.465 0 0 1-1.235 4.125h-.148c-.806 0-1.534.446-2.031 1.08a9.04 9.04 0 0 1-2.861 2.4c-.723.384-1.35.956-1.653 1.715a4.499 4.499 0 0 0-.322 1.672v.633A.75.75 0 0 1 9 22a2.25 2.25 0 0 1-2.25-2.25c0-1.152.26-2.243.723-3.218.266-.558-.107-1.282-.725-1.282H3.622c-1.026 0-1.945-.694-2.054-1.715A12.137 12.137 0 0 1 1.5 12.25c0-2.848.992-5.464 2.649-7.521C4.537 4.247 5.136 4 5.754 4H9.77a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23ZM21.669 14.023c.536-1.362.831-2.845.831-4.398 0-1.22-.182-2.398-.52-3.507-.26-.85-1.084-1.368-1.973-1.368H19.1c-.445 0-.72.498-.523.898.591 1.2.924 2.55.924 3.977a8.958 8.958 0 0 1-1.302 4.666c-.245.403.028.959.5.959h1.053c.832 0 1.612-.453 1.918-1.227Z" />
         </svg>
@@ -108,9 +115,7 @@ function Home() {
               <img onClick={handleClick} src={scissor} alt="scissor" className="size-full object-cover" />
             </div>
           </div>
-          {
-        scoreCountUser
-      }
+          <p className='text-center '>{scoreCountUser}</p>
         </div>
         <div className="text-4xl font-bold text-gray-800">vs</div>
         <div className='flex flex-col gap-6'>
@@ -133,10 +138,23 @@ function Home() {
               <div className="absolute top-0 bottom-0 left-0 right-0 bg-gray-400/50"></div>
             </div>
           </div>
-          {scoreCountSystem}
+          <p className='text-center '>{scoreCountSystem}</p>
         </div>
       </div>
+      <div className='w-full flex items-center justify-center'>
+        <table className='table-auto'>
+          <tr>
+            <th className='p-2 border'>user</th>
+            <th className='p-2 border'>system</th>
+          </tr>
+          <tr>
+            <td className='p-2 border text-center'>{scoreBoard.user}</td>
+            <td className='p-2 border text-center'>{scoreBoard.system}</td>
+          </tr>
+        </table>
+      </div>
     </div>
+
   );
 }
 
